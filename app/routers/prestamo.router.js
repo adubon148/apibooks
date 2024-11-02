d37dd8a50d0f1215f208c45d8d5b58458f02ea77
adubon148 <adubonb1@miumg.edu.gt>
2024-11-02 12:12:31 -0600

    fixroter2

--- app/routers/prestamo.router.js
+++ app/routers/prestamo.router.js
@@ -2,7 +2,7 @@
 let express = require('express');
 let router = express.Router();
  
-const prstm = require('../controllers/prestamo.controller');
+const prstm = require('../controllers/prestamo.controller.js');
 
 router.post('/api/pretm/crear', prstm.create);
 router.get('/api/pretm/all', prstm.retrieveAll);
@@ -10,4 +10,4 @@ router.get('/api/pretm/onebyid/:id', prstm.getById);
 router.put('/api/pretm/update/:id', prstm.updateById);
 router.delete('/api/pretm/delete/:id',prstm.deleteById);
 
-module.exports = prstm;
+module.exports = router;
